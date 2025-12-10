f1f8c967a14742047c779abb765f962c5e1643f4
Alexandre <alexandre.guerrier@isen-ouest.yncrea.fr>
2025-12-10 16:07:29 +0100

    ng content et outlet

--- src/components/home-page/home-page.ts
+++ src/components/home-page/home-page.ts
@@ -1,8 +1,10 @@
 import { Component } from '@angular/core';
+import { Layout } from "../layout/layout";
+import { RouterOutlet } from "../../../node_modules/@angular/router/types/_router_module-chunk";
 
 @Component({
   selector: 'app-home-page',
-  imports: [],
+  imports: [Layout, RouterOutlet],
   templateUrl: './home-page.html',
   styleUrl: './home-page.scss',
 })
