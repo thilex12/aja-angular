eade70e7208791a07e831fb30a667840fc65192b
Alexandre <alexandre.guerrier@isen-ouest.yncrea.fr>
2025-12-10 15:57:38 +0100

    layout

--- src/components/layout/layout.ts
+++ src/components/layout/layout.ts
@@ -2,9 +2,10 @@ import {Component} from '@angular/core';
 import {MatToolbarModule} from '@angular/material/toolbar';
 import {MatIconModule} from '@angular/material/icon';
 import {MatButtonModule} from '@angular/material/button';
+import { RouterLink } from "@angular/router";
 @Component({
   selector: 'app-layout',
-  imports: [MatToolbarModule, MatIconModule, MatButtonModule],
+  imports: [MatToolbarModule, MatIconModule, MatButtonModule, RouterLink],
   templateUrl: './layout.html',
   styleUrl: './layout.scss',
 })
