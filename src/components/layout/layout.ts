f0a213b94315bd676e7db33724ca258f3cee8095
Alexandre <alexandre.guerrier@isen-ouest.yncrea.fr>
2025-12-10 16:18:39 +0100

    fix import router outlet

--- src/components/layout/layout.ts
+++ src/components/layout/layout.ts
@@ -2,10 +2,10 @@ import {Component} from '@angular/core';
 import {MatToolbarModule} from '@angular/material/toolbar';
 import {MatIconModule} from '@angular/material/icon';
 import {MatButtonModule} from '@angular/material/button';
-import { RouterLink, RouterOutlet } from "@angular/router";
+import { RouterLink } from "@angular/router";
 @Component({
   selector: 'app-layout',
-  imports: [MatToolbarModule, MatIconModule, MatButtonModule, RouterLink, RouterOutlet],
+  imports: [MatToolbarModule, MatIconModule, MatButtonModule, RouterLink],
   templateUrl: './layout.html',
   styleUrl: './layout.scss',
 })
