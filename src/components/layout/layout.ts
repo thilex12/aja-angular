96c80125f33f2d7362f2f10c1c8578788ef6974d
Alexandre <alexandre.guerrier@isen-ouest.yncrea.fr>
2025-12-10 15:58:55 +0100

    router outlet

--- src/components/layout/layout.ts
+++ src/components/layout/layout.ts
@@ -2,10 +2,10 @@ import {Component} from '@angular/core';
 import {MatToolbarModule} from '@angular/material/toolbar';
 import {MatIconModule} from '@angular/material/icon';
 import {MatButtonModule} from '@angular/material/button';
-import { RouterLink } from "@angular/router";
+import { RouterLink, RouterOutlet } from "@angular/router";
 @Component({
   selector: 'app-layout',
-  imports: [MatToolbarModule, MatIconModule, MatButtonModule, RouterLink],
+  imports: [MatToolbarModule, MatIconModule, MatButtonModule, RouterLink, RouterOutlet],
   templateUrl: './layout.html',
   styleUrl: './layout.scss',
 })
