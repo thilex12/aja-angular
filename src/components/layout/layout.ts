f083478db9780549092615f86f43f0073976396e
Alexandre <alexandre.guerrier@isen-ouest.yncrea.fr>
2025-12-10 15:55:49 +0100

    layout

--- src/components/layout/layout.ts
+++ src/components/layout/layout.ts
@@ -1,9 +1,10 @@
 import {Component} from '@angular/core';
 import {MatToolbarModule} from '@angular/material/toolbar';
-
+import {MatIconModule} from '@angular/material/icon';
+import {MatButtonModule} from '@angular/material/button';
 @Component({
   selector: 'app-layout',
-  imports: [MatToolbarModule],
+  imports: [MatToolbarModule, MatIconModule, MatButtonModule],
   templateUrl: './layout.html',
   styleUrl: './layout.scss',
 })
