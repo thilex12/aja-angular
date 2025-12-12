ded7bc06a9c99a6b9bd54b61009a75fd6f6f5172
jean <jean.bayard@isen-ouest.yncrea.fr>
2025-12-12 17:31:25 +0100

    Modification de pour ajouter des marges

--- src/components/layout/layout.ts
+++ src/components/layout/layout.ts
@@ -3,9 +3,11 @@ import {MatToolbarModule} from '@angular/material/toolbar';
 import {MatIconModule} from '@angular/material/icon';
 import {MatButtonModule} from '@angular/material/button';
 import { RouterLink } from "@angular/router";
+import { MatDividerModule } from '@angular/material/divider';
+
 @Component({
   selector: 'app-layout',
-  imports: [MatToolbarModule, MatIconModule, MatButtonModule, RouterLink],
+  imports: [MatToolbarModule, MatIconModule, MatButtonModule, RouterLink, MatDividerModule],
   templateUrl: './layout.html',
   styleUrl: './layout.scss',
 })
