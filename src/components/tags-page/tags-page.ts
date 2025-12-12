d937c13fbe0e7c1ed1bc67269a72e2e487c47526
jean <jean.bayard@isen-ouest.yncrea.fr>
2025-12-12 11:43:03 +0100

    Ajout du design de base sur events-page, localisations-page, tags-page, users-page

--- src/components/tags-page/tags-page.ts
+++ src/components/tags-page/tags-page.ts
@@ -1,8 +1,14 @@
 import { Component } from '@angular/core';
+import { MatCardModule } from '@angular/material/card';
+import { Layout } from "../layout/layout";
+import { RouterOutlet } from "@angular/router";
+import {MatExpansionModule} from '@angular/material/expansion';
+import {MatDividerModule} from '@angular/material/divider';
+import {MatListModule} from '@angular/material/list';
 
 @Component({
   selector: 'app-tags-page',
-  imports: [],
+  imports: [Layout, MatCardModule, RouterOutlet, MatExpansionModule, MatDividerModule, MatListModule],
   templateUrl: './tags-page.html',
   styleUrl: './tags-page.scss',
 })
