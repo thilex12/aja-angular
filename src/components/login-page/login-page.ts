d47ca5854ea5f2d1d9c2f2e837f141fd5323fcdf
Alexandre <alexandre.guerrier@isen-ouest.yncrea.fr>
2025-12-12 16:02:24 +0100

    api trick by le prof

--- src/components/login-page/login-page.ts
+++ src/components/login-page/login-page.ts
@@ -1,9 +1,10 @@
-import { Component } from '@angular/core';
+import { Component, inject } from '@angular/core';
 import { FormsModule, NgForm } from '@angular/forms';
 import { MatButtonModule } from '@angular/material/button'
 import { MatIconModule } from "@angular/material/icon";
 import { MatInputModule } from '@angular/material/input';
 import { MatFormFieldModule } from '@angular/material/form-field';
+import { WhatTimeApi } from '../../services/what-time-api';
 
 
 @Component({
@@ -14,12 +15,21 @@ import { MatFormFieldModule } from '@angular/material/form-field';
 })
 export class LoginPage {
 
+  api = inject(WhatTimeApi);
+
   email: string = '';
   password: string = '';
+
+
 
   login(form: NgForm) {
     this.email = form.value.email;
     this.password = form.value.password;
+    this.api.getInfo(this.email, this.password).subscribe((response) => {
+      console.log(this.api.user()?.role);
+    });
+
+
     // Implement login logic here
 
   }
