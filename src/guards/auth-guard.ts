140215e7a77b3a9ab050d6eabf57fecf4384e7b1
Alexandre <alexandre.guerrier@isen-ouest.yncrea.fr>
2025-12-12 17:00:21 +0100

    localStorage loginPage et auth guard

--- src/guards/auth-guard.ts
+++ src/guards/auth-guard.ts
@@ -3,8 +3,10 @@ import { CanActivateFn, Router } from '@angular/router';
 
 export const authGuard: CanActivateFn = (route, state) => {
   // Verfier si l'utilisateur est authentifié
-  const isAuthenticated = document.cookie.includes('email') && document.cookie.includes('password');
+  // const isAuthenticated = document.cookie.includes('email') && document.cookie.includes('password');
+  // const password = document.cookie.split('; ').find(row => row.startsWith('password='))?.split('=')[1];
   // return isAuthenticated;
+  const isAuthenticated = localStorage.getItem('email') !== null && localStorage.getItem('password') !== null;
   if (isAuthenticated) {
     return true;
   } else {
