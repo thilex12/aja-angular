54677e6b71dcdec3f1f8d832794b5d3af12a3c02
Gezoo64 <peyrachearnaud@gmail.com>
2025-12-12 15:45:12 +0100

    correction mineure what time api (call) + création du modèle user-details-module

--- src/components/test-arnaud/test-arnaud.ts
+++ src/components/test-arnaud/test-arnaud.ts
@@ -1,8 +1,5 @@
-import { Component, inject, signal } from '@angular/core';
-import { ApiCallService } from "../../services/api-call-service";
-import { CookieService } from "../../services/cookie-service";
-import { EventDetailsModel, Page } from '../../models/event-details/event-details-module';
-import { tap } from 'rxjs';
+import { Component, OnInit, inject, signal } from '@angular/core';
+import { WhatTimeApi } from '../../services/what-time-api';
 
 @Component({
   selector: 'app-test-arnaud',
@@ -10,27 +7,13 @@ import { tap } from 'rxjs';
   templateUrl: './test-arnaud.html',
   styleUrl: './test-arnaud.scss',
 })
-export class TestArnaud {
-  protected api = inject(ApiCallService);
-  protected cookies = inject(CookieService);
-  protected events = signal<EventDetailsModel[]>([])
+export class TestArnaud implements OnInit{
+  protected api = inject(WhatTimeApi);
 
-  
-
+  ngOnInit() {
+    this.api.getEvents();
+  }
   protected test() {
-    let cookies = this.cookies.get();
-    let username = cookies.get("username");
-    let pwd = cookies.get("password");
-    this.api.get<Page<EventDetailsModel>>("/admin-events", username, pwd).subscribe(r=>{
-      console.log(r.content);
-      this.events.set(r.content);
-      console.log(this.events()[0]);
-    });
-    this.api.get<Page<EventDetailsModel>>("/admin-events", username, pwd).subscribe(r=>{
-      console.log(r.content);
-      this.events.set(r.content);
-      console.log(this.events()[0]);
-    });
-    
+    console.log(this.api.events());
   }
 }
