3f50711392b31c1174d03897e542c565eaaa0c42
jean <jean.bayard@isen-ouest.yncrea.fr>
2025-12-12 11:43:03 +0100

    Ajout du design de base sur events-page, localisations-page, tags-page, users-page

--- src/components/users-page/users-page.ts
+++ src/components/users-page/users-page.ts
@@ -2,14 +2,17 @@ import { Component } from '@angular/core';
 import { MatCardModule } from '@angular/material/card';
 import { Layout } from "../layout/layout";
 import { RouterOutlet } from "@angular/router";
+import {MatExpansionModule} from '@angular/material/expansion';
+import {MatDividerModule} from '@angular/material/divider';
+import {MatListModule} from '@angular/material/list';
+
 
 @Component({
   selector: 'app-users-page',
-  imports: [MatCardModule, Layout, RouterOutlet],
+  imports: [MatCardModule, Layout, RouterOutlet, MatExpansionModule, MatDividerModule, MatListModule],
   templateUrl: './users-page.html',
   styleUrl: './users-page.scss',
 })
 export class UsersPage {
-  protected longText: string = `The Chihuahua is a breed of small dog named after the state of Chihuahua in Mexico. The breed is believed to have descended from an ancient dog breed known as the Techichi, which was kept by the Toltec civilization in Mexico. Chihuahuas are known for their small size, large ears, and big personality. They are often described as lively, alert, and loyal companions. Despite their small stature, Chihuahuas can be quite bold and confident. They come in a variety of colors and coat types, including smooth and long-haired varieties. Due to their size, they are well-suited for apartment living and can be great pets for individuals or families looking for a small dog with a big heart.`;
-
+  protected listTags = ['tag 1', 'tag 2', 'tag 3',]
 }
