d799a1bc6b14b7c8c1be028283412d054481ea2e
jean <jean.bayard@isen-ouest.yncrea.fr>
2025-12-18 15:42:25 +0100

    ajout d'un loading et dialog fonctionnel

--- src/components/users-dialog/users-dialog.ts
+++ src/components/users-dialog/users-dialog.ts
@@ -1,13 +1,14 @@
 import { Component, inject } from '@angular/core';
 import { MatDialogModule, MatDialogRef } from '@angular/material/dialog';
 import {FormsModule, NgForm} from '@angular/forms';
+import { MatInputModule } from '@angular/material/input';
 import {MatFormFieldModule} from '@angular/material/form-field';
 import { WhatTimeApi } from '../../services/what-time-api';
 
 
 @Component({
   selector: 'app-users-dialog',
-  imports: [MatDialogModule, FormsModule, MatFormFieldModule],
+  imports: [MatDialogModule, FormsModule, MatFormFieldModule, MatInputModule],
   templateUrl: './users-dialog.html',
   styleUrl: './users-dialog.scss',
 })
@@ -20,18 +21,22 @@ export class UsersDialog {
   protected api = inject(WhatTimeApi);
 
   create(form: NgForm) {
-  this.mail = form.value.email;
-  this.password = form.value.password;
-  this.api.getInfo(this.mail, this.password).subscribe((response) => {
-    console.log(this.api.user()?.role);
-  });
+  // this.mail = form.value.email;
+  // this.password = form.value.password;
+  // this.api.getInfo(this.mail, this.password).subscribe((response) => {
+  //   this.api.createUser(this.name, this.surname, this.mail, btoa(this.password)).subscribe(() => {
+  //     this.dialogRef.close();
+  //   }
+  // );
+  // });
+    this.password = btoa(form.value.password);
+    this.mail = form.value.mail;
+    this.name = form.value.name;
+    this.surname = form.value.surname;
+    this.dialogRef.close([this.password, this.mail, this.name, this.surname]);
 }
 
   onCancelClick(): void {
-    this.dialogRef.close();
-  }
-
-  onCreateClick(): void {
-    this.dialogRef.close();
+    this.dialogRef.close('cancelled');
   }
 }
--- src/components/users-page/users-page.ts
+++ src/components/users-page/users-page.ts
@@ -8,7 +8,6 @@ import { MatListModule } from '@angular/material/list';
 // import { UserModule } from '../../models/user/user-module';
 import { WhatTimeApi } from '../../services/what-time-api';
 import { UserModel } from '../../models/user/user-module';
-import { Page } from '../../models/page/page-module';
 import { UserDetailsModel } from '../../models/user-details/user-details-module';
 import { MatIconModule } from '@angular/material/icon';
 import { MatButtonModule } from '@angular/material/button';
@@ -31,11 +30,16 @@ export class UsersPage implements OnInit {
   users = signal<UserModel[]>([]);
   userDetails = signal<UserDetailsModel | null>(null);
   tags = signal(JSON.parse(localStorage.getItem('tags') || '[]'));
+  protected loadingPage = signal<boolean>(true);
+  protected loadingUser = signal<boolean>(true);
+  protected dialog = inject(MatDialog);
 
 
   getUserDetails(id: number) {
+    this.loadingUser.set(true);
     this.api.getUserById(id).subscribe((response) => {
       this.userDetails.set(response);
+      this.loadingUser.set(false);
       // console.log(response);
     });
   }
@@ -51,17 +55,19 @@ export class UsersPage implements OnInit {
         // console.log("Response.content:", response);
         this.users.set(response);
         // console.log("Users() après set:", this.users());
+        this.loadingPage.set(false);
       },
       error: (err) => {
         console.error("Erreur API:", err);
       }
     });
   }
-  protected loading: boolean = true;
-  protected dialog = inject(MatDialog);
 
-openDialog(): void {
+  openDialog(): void {
 
-  const dialogRef = this.dialog.open(UsersDialog, {});
-}
+    const dialogRef = this.dialog.open(UsersDialog, {});
+    dialogRef.afterClosed().subscribe(result => {
+      console.log(result);
+    });
+  }
 }
